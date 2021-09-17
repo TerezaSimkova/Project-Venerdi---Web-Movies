1b82a8445422ca22d3c04b9d496d518098ddf2a1
TerezaSimkova <71644745+TerezaSimkova@users.noreply.github.com>
2021-09-17 15:34:49 +0200

    Add files via upload

---
+++ ProvaVenerdi/JavaScript.js
@@ -0,0 +1,67 @@
+﻿
+
+
+function saveData() {
+    let email = document.getElementById("email").value;
+
+    localStorage.setItem("email", email);
+}
+
+function ChangeButton() {
+
+    let inputs = document.querySelectorAll("input[type='text']");
+
+    let subscribeBtn = document.querySelector("input[type='submit']");
+
+    for (let i = 0; i < inputs.length; i++) {
+        if (inputs[i].value == "") 
+        {
+            subscribeBtn.disabled = true;
+            return;
+        }       
+    }
+    subscribeBtn.disabled = false;
+}
+function hideForm() {
+   
+    let form = document.getElementById("Form");
+
+    form.style.display = "none"; 
+
+   
+    let unsButton = document.getElementById("btn");
+
+    unsButton.style.display = "";
+}
+
+function checkEmail() {
+
+    let storedEmail = localStorage.getItem('email');
+
+    if (storedEmail != null) {
+        hideForm();
+    }
+
+}
+
+function unsubscribeMe() {
+
+    localStorage.clear();
+
+    let form = document.getElementById("Form");
+    form.style.display = "";
+
+    let unsButton = document.getElementById("btn");
+    unsButton.style.display = "none";
+
+}
+
+function Benvenuto() {
+
+    let storedEmail = localStorage.getItem('email');
+
+    if (storedEmail != null) {
+        alert("***Benvenuto!***")
+    }
+
+}
